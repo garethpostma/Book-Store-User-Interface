867790f42b48df3d03d4e866834a723dba637b0f
garethpostma <gpostma14@gmail.com>
2017-01-21 12:00:21 -0500

    Update eBook.java

--- eBook.java
+++ eBook.java
@@ -1,12 +1,4 @@
-/* Name: Gareth Postma
- * MacID: postmagn
- * Student Number: 001422248
- * Name: Noah Zwiep
- * MacID: zwiepn
- * Student Number: 001424643
- * Name: Mitchell Overbeeke
- * MacID: overbeml
- * Student Number: 001422018
+/* 
  * Description: this class sets the info for item of type ebook
  * */
 public class eBook extends Readable{
