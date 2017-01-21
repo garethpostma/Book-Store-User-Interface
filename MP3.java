d21e2318051090819baf46a8dd5b051587f7bada
garethpostma <gpostma14@gmail.com>
2017-01-21 11:58:07 -0500

    Update MP3.java

--- MP3.java
+++ MP3.java
@@ -1,12 +1,4 @@
-/* Name: Gareth Postma
- * MacID: postmagn
- * Student Number: 001422248
- * Name: Noah Zwiep
- * MacID: zwiepn
- * Student Number: 001424643
- * Name: Mitchell Overbeeke
- * MacID: overbeml
- * Student Number: 001422018
+/* 
  * Description: this class has the functions for an Item of type mps
  * */
 public class MP3 extends Audio{ //this class extends the class audio
